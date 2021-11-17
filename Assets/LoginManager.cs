72e7c61b6013d6bf687fcc3ff4574a71174e032c
Morgan Ethier <morganethier@gmail.com>
2021-11-17 15:44:35 -0500

    Added support for LoginToggle and GameRoom dropdown

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
@@ -14,6 +14,8 @@ public class LoginManager : MonoBehaviour
 
     //Member Variables
     GameObject networkedClient;
+    int currentGameRoom = -1;
+    bool isLogin = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +23,86 @@ public class LoginManager : MonoBehaviour
         GameObject[] sceneObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
         foreach(GameObject go in sceneObjects)
         {
-
+            if (go.name == "UsernameField")
+            {
+                usernameField = go;
+            }
+            else if (go.name == "PasswordField")
+            {
+                passwordField = go;
+            }
+            else if(go.name == "GameRoomField")
+            {
+                gameRoomField = go;
+            }
+            else if(go.name == "Login/CreateAccount")
+            {
+                loginToggle = go;
+            }
+            else if(go.name == "Submit")
+            {
+                submitButton = go;
+            }
         }
+
+        submitButton.GetComponent<Button>().onClick.AddListener(SubmitButtonPressed);
+        loginToggle.GetComponent<Button>().onClick.AddListener(ToggleButtonPressed);
+
+        //gameRoomField.GetComponent<Dropdown>().onValueChanged.AddListener(UpdateGameRoom);
+
+        UpdateGameRoom();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SubmitButtonPressed()
+    {
+
+    }
+
+    //Switch between Login and Account Creation
+    public void ToggleButtonPressed()
+    {
+        if(isLogin)
+        {
+            loginToggle.GetComponent<Button>().GetComponentInChildren<Text>().text = "Create Account";
+            gameRoomField.SetActive(false);
+        }
+        else if(!isLogin)
+        {
+            loginToggle.GetComponent<Button>().GetComponentInChildren<Text>().text = "Login";
+            gameRoomField.SetActive(true);
+        }
+
+        UpdateGameRoom();
+        isLogin = !isLogin;
+    }
+
+    public void UpdateGameRoom()
+    {
+        if(isLogin)
+        {
+            if(gameRoomField.GetComponent<Dropdown>().captionText.text == "Game Room 1")
+            {
+                currentGameRoom = 1;
+            }
+            else if(gameRoomField.GetComponent<Dropdown>().captionText.text == "Game Room 2")
+            {
+                currentGameRoom = 2;
+            }
+            else if (gameRoomField.GetComponent<Dropdown>().captionText.text == "Game Room 3")
+            {
+                currentGameRoom = 3;
+            }
+        }
+        else 
+        {
+            currentGameRoom = -1;
+        }
     }
 }
